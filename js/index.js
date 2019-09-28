3cfe82b83d0aaebfc0c648fc7735c321d2844724
Juan Jose <juan.josecuadros1@gmail.com>
2019-09-28 16:54:17 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+      apiKey: "AIzaSyDcdNGGTv5ReUfpNEBCBy7Eb-73YkB0KPw",
+      authDomain: "paletas-7bdc3.firebaseapp.com",
+      databaseURL: "https://paletas-7bdc3.firebaseio.com",
+      projectId: "paletas-7bdc3",
+      storageBucket: "",
+      messagingSenderId: "1029428786326",
+      appId: "1:1029428786326:web:eb4321da1f52af7ac03eb7",
+      measurementId: "G-S9P7Z4STK1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
