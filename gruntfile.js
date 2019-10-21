0c52c185707c603bf2914546ede700bde8a50f18
Juan Jose <juan.josecuadros1@gmail.com>
2019-10-20 22:26:02 -0500

    setup Grunt tasks for minifying and compiling scss files into main.scss

--- gruntfile.js
+++ gruntfile.js
@@ -1,21 +1,32 @@
 module.exports = function(grunt) {
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         sass: {
-            dist: {
-                files: {
-                    'public/css/main.css': '**/*.scss'
-                }
+            build: {
+                files: [{
+                    src: 'app/sass/main.scss',
+                    dest: 'public/css/main.css'
+                }]
+            }
+        },
+        cssmin: {
+            target: {
+                src: "public/css/main.css",
+                dest: "public/css/main.min.css"
             }
         },
         watch: {
             css: {
-                files: '**/*.scss',
-                tasks: ['sass']
+                files: ['app/**/*.scss'],
+                tasks: ['sass', 'cssmin']
             }
-        }
+        },
     });
+
+
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['watch']);
 }
